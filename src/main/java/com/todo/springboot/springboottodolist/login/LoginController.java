072c41b53de7f06f176a13327f143e49f54423b5
nitish2101 <nitishsharma50@gmail.com>
2023-09-07 12:29:03 +0530

    todo page added

--- src/main/java/com/todo/springboot/springboottodolist/login/LoginController.java
+++ src/main/java/com/todo/springboot/springboottodolist/login/LoginController.java
@@ -1,16 +1,34 @@
 package com.todo.springboot.springboottodolist.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.todo.springboot.springboottodolist.AuthenticationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    @RequestMapping("/login")
+    private AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @RequestMapping(value="/login",method = RequestMethod.GET)
     public String loginJsp(){
         return "login";
     }
 
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
+        if(authenticationService.authenticate(name, password)) {
+            model.put("name", name);
+            return "welcome";
+        }
+        model.put("errorMessage","Invalid Username or Pasword");
+        return "login";
+    }
 }
